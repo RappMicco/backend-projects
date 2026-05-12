9da6e1591e598c6455cdf99a8375c283cf296c6a
Rapp <rapp.miccorizo@gmail.com>
2026-05-12 21:39:06 +0800

    chore: refactor

--- final-portfolio-backend/middleware/limiter.js
+++ final-portfolio-backend/middleware/limiter.js
@@ -2,9 +2,9 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 10 * 30 * 1000, //3 minutes
-    max: 5,
+    max: 3,
     message: {
         success: false,
-        message: "You reached the limit (2 requests). Please try again later!"
+        message: "You reached the limit (3 requests). Please try again later!"
     }
 });
